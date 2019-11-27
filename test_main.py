4fc366565c7a1033f9070dd4187b23e81a9dc465
Niduroki <chris@niduroki.net>
2019-11-27 15:56:38 +0100

    Implement anti-cheat unittest

--- test_main.py
+++ test_main.py
@@ -1,13 +1,25 @@
+import os
+import tempfile
+from itertools import product
 import pytest
-from main import app
+from freezegun import freeze_time
+from datetime import datetime, timedelta
+
+from main import app, db
 
 
 @pytest.fixture
 def client():
-    app.config['DATABASE'] = "sqlite://"
+    db_fd, db_file = tempfile.mkstemp()
+    app.config['DATABASE'] = "sqlite://" + db_file
     app.config['TESTING'] = True
     with app.test_client() as client:
+        with app.app_context():
+            db.get_session()  # Initialize db
         yield client
+
+    os.close(db_fd)
+    os.unlink(db_file)
 
 
 # TODO Zeitzonen Krams sowohl mit Sommer als auch Winterzeit testen (also wahrscheinlich freezegun aus Projekt Play)
@@ -20,11 +32,67 @@ def test_timezone_submit(client):
 
 
 def test_cheater_prevention(client):
-    #TODO
-    # feld erstellen, sofort fünf Kreuze machen -> Fehler, Feld wird gelöscht
-    # dito nochmal mit 5 kreuzen nach 1:55 Stunden
-    # dito mit 2 kreuzen nach 1:55 und drei kreuzen nach 2:05, die müssen durchgehen!
-    pass
+    """ Check whether fields finished within 2 hours are detected as cheaters """
+
+    # Create a field
+    rv = client.post('/', data=dict(player_name="cheater"))
+    game_link = rv.location[-11:-1]
+    session = db.get_session()
+    game_pk = session.query(db.BingoField.id).filter(db.BingoField.link == game_link).one()[0]
+    rv = client.get(f'/{game_link}/')
+    assert b'Spieler: cheater' in rv.data
+    # Tick five fields right now
+    for x, y in product((1, 1, 1, 1), (1, 2, 3, 4)):
+        rv = client.post(f'/{game_link}/submit/{x}/{y}/')
+        assert {'data': 'success', 'x': x, 'y': y} == rv.get_json()
+    rv = client.post(f'/{game_link}/submit/1/5/')
+    assert {'data': 'cheater'} == rv.get_json()
+    # Check if field has been deleted
+    assert 0 == session.query(db.BingoField).filter(db.BingoField.id == game_pk).count()
+    assert 0 == session.query(db.BingoSquares).filter(db.BingoSquares.bingo_field_id == game_pk).count()
+
+    # Tick five fields after 1:55 hours
+    with freeze_time(datetime.now()) as frozen_time:
+        rv = client.post('/', data=dict(player_name="cheater155"))
+        game_link = rv.location[-11:-1]
+        session = db.get_session()
+        game_pk = session.query(db.BingoField.id).filter(db.BingoField.link == game_link).one()[0]
+        rv = client.get(f'/{game_link}/')
+        assert b'Spieler: cheater155' in rv.data
+        # Wait 1:55 hours
+        frozen_time.tick(delta=timedelta(hours=1, minutes=55))
+        # Tick five fields now
+        for x, y in product((1, 1, 1, 1), (1, 2, 3, 4)):
+            rv = client.post(f'/{game_link}/submit/{x}/{y}/')
+            assert {'data': 'success', 'x': x, 'y': y} == rv.get_json()
+        rv = client.post(f'/{game_link}/submit/1/5/')
+        assert {'data': 'cheater'} == rv.get_json()
+        # Check if field has been deleted
+        assert 0 == session.query(db.BingoField).filter(db.BingoField.id == game_pk).count()
+        assert 0 == session.query(db.BingoSquares).filter(db.BingoSquares.bingo_field_id == game_pk).count()
+
+        # Tick four fields after 1:55 hours and fifth after 2:05 hours – Should pass
+        with freeze_time(datetime.now()) as frozen_time:
+            rv = client.post('/', data=dict(player_name="legit"))
+            game_link = rv.location[-11:-1]
+            session = db.get_session()
+            game_pk = session.query(db.BingoField.id).filter(db.BingoField.link == game_link).one()[0]
+            rv = client.get(f'/{game_link}/')
+            assert b'Spieler: legit' in rv.data
+            # Wait 1:55 hours
+            frozen_time.tick(delta=timedelta(hours=1, minutes=55))
+            # Tick four fields now
+            for x, y in product((1, 1, 1, 1), (1, 2, 3, 4)):
+                rv = client.post(f'/{game_link}/submit/{x}/{y}/')
+                assert {'data': 'success', 'x': x, 'y': y} == rv.get_json()
+            # Wait 10 minutes
+            frozen_time.tick(delta=timedelta(minutes=10))
+            rv = client.post(f'/{game_link}/submit/1/5/')
+            assert {'data': 'finished', 'score': (1000000/125)} == rv.get_json()
+            # Check if field is finished
+            game = session.query(db.BingoField).filter(db.BingoField.id == game_pk).one()
+            assert game.score == 1000000 / 125
+            assert game.finished
 
 
 def test_bingo_scoring(client):
